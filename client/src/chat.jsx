3596b636f0e0ad486cb486f8c529da983d9d4b8a
yash-shukla-2004 <160156358+yash-shukla-2004@users.noreply.github.com>
2024-08-23 07:38:54 +0530

    updated_routes
    Updated the routes for Login and SignUP

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./chat.pages/home/Home.jsx";
 import Login from "./chat.pages/login/Login.jsx";
 import SignUp from "./chat.pages/signup/SignUp.jsx";
+import Defhoc from "./HOC/default_hoc";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./chat.context/AuthContext.jsx";
 
@@ -11,9 +12,9 @@ function App() {
 	return (
             <div className='p-4 h-screen flex items-center justify-center'>
                 <Routes>
-                    <Route path='/' element={authUser ? <Home /> : <Navigate to="chat/login" />} />
-                    <Route path='chat/login' element={authUser ? <Navigate to='chat/' /> : <Login />} />
-                    <Route path='chat/signup' element={authUser ? <Navigate to='chat/' /> : <SignUp />} />
+                    <Route path='/' element={authUser ? <Home /> : <Defhoc temp ={Login} /> } />
+                    <Route path='/login' element={authUser ? <Navigate to='chat/' /> : <Login />} />
+                    <Route path='/signup' element={authUser ? <Navigate to='chat/' /> : <SignUp />} />
                 </Routes>
                 <Toaster />
             </div>
--- client/src/chat.pages/signup/SignUp.jsx
+++ client/src/chat.pages/signup/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='John Doe'
+							placeholder='Enter Name'
 							className='w-full input input-bordered  h-10'
 							value={inputs.fullName}
 							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
@@ -50,7 +50,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='johndoe'
+							placeholder='Enter Username'
 							className='w-full input input-bordered h-10'
 							value={inputs.username}
 							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
@@ -86,7 +86,7 @@ const SignUp = () => {
 					<GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
 
 					<Link
-						to={"/login"}
+						to={"/chat/login"}
 						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
 						href='#'
 					>
